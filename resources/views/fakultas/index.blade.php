0c863119edc46cd8277b631c04a88a0a88ea5cbb
Vanika Ipsandy <vanikaipsandy982@gmail.com>
2021-06-04 23:06:04 +0700

    ubah jadi edit

--- resources/views/fakultas/index.blade.php
+++ resources/views/fakultas/index.blade.php
@@ -67,7 +67,7 @@
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalCenterTitle">Ubah Fakultas</h5>
+                <h5 class="modal-title" id="exampleModalCenterTitle">Edit Fakultas</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -76,9 +76,8 @@
                 <form>
                     <!--Nama Fakultas-->
                     <div class="form-group">
-                        <label for="nama_fakultas">Nama Fakultas
+                        <label for="nama_fakultas">Nama Fakultas</label>
                         <input type="text" class="form-control" placeholder="Nama Fakultas">
-                        </label>
                     </div>
                     <!--Button Simpan-->
                     <div class="modal-footer">
